3f2515fccda6927402afc0d43a56ea3b2d5b78fd
MinMax25 <kinocta@qa2.so-net.ne.jp>
2025-12-29 13:09:45 +0900

    reg

--- source/cchordselecterfactory.cpp
+++ source/cchordselecterfactory.cpp
@@ -12,6 +12,67 @@
 namespace MinMax
 {
     using namespace VSTGUI;
+    class ChordSelectorListner
+        : public OptionMenuListenerAdapter
+    {
+    public:
+        ChordSelectorListner(VST3Editor* editor)
+            : editor(editor)
+        {
+        }
+
+        bool onOptionMenuSetPopupResult(COptionMenu* menu, COptionMenu* selectedMenu, int32_t selectedIndex) override
+        {
+            if (!menu || !selectedMenu) return false;
+
+            CChord chord;
+
+            bool finish = false;
+            int chordNumber = 0; // 先頭からの位置インデックス
+
+            for (int r = 0; r < menu->getNbEntries(); ++r)
+            {
+                COptionMenu* typeMenu = menu->getSubMenu(r);
+                if (!typeMenu) continue;
+
+                for (int t = 0; t < typeMenu->getNbEntries(); ++t)
+                {
+                    COptionMenu* posMenu = typeMenu->getSubMenu(t);
+                    if (!posMenu) continue;
+
+                    if (posMenu == selectedMenu)
+                    {
+                        chord.root = r;
+                        chord.type = t;
+                        chord.position = selectedIndex;
+
+                        chordNumber += selectedIndex;
+
+                        finish = true;
+                    }
+                    if (finish) break;
+                    chordNumber += posMenu->getNbEntries();
+                }
+                if (finish) break;
+            }
+
+            if (finish)
+            {
+                EditController* controller = dynamic_cast<EditController*>(editor->getController());
+                if (!controller) return false;
+                controller->beginEdit(1104);
+                ParamValue norm = controller->plainParamToNormalized(1104, chordNumber);
+                controller->setParamNormalized(1104, norm);
+                controller->performEdit(1104, norm);
+                controller->endEdit(1104);
+            }
+
+            return finish;
+        }
+
+    private:
+        VST3Editor* editor = nullptr;
+    };
 
     class CChordSelecter
         : public CViewContainer
@@ -22,46 +83,57 @@ namespace MinMax
             : CViewContainer(size)
         {
             editor = static_cast<VST3Editor*>(description->getController());
-            editor->addRef();
+            if (editor) editor->addRef();
+
+            // 階層化メニュー
+            hierMenu = new COptionMenu(CRect(0, 1, 39, 18), nullptr, -1);
+            menuListener = std::make_unique<ChordSelectorListner>(editor);
+
+            hierMenu->setFontColor(kWhiteCColor);
+            hierMenu->setBackColor(kGreyCColor);
+            hierMenu->registerOptionMenuListener(menuListener.get());
+
+            // ChordMap から階層データを追加
+            const auto& chordMap = ChordMap::Instance();
+            auto rootNames = chordMap.getRootNames();
+
+            for (int r = 0; r < (int)rootNames.size(); ++r)
+            {
+                COptionMenu* typeMenuSub = new COptionMenu(CRect(0, 0, 150, 20), nullptr, -1);
+
+                auto chordNames = chordMap.getChordNames(r);
+                for (int t = 0; t < (int)chordNames.size(); ++t)
+                {
+                    COptionMenu* posMenuSub = new COptionMenu(CRect(0, 0, 150, 20), nullptr, -1);
+
+                    auto posNames = chordMap.getFretPosNames(r, t);
+                    for (auto& posName : posNames)
+                    {
+                        posMenuSub->addEntry(posName.c_str());
+                    }
+
+                    typeMenuSub->addEntry(posMenuSub, chordNames[t].c_str());
+                }
+
+                hierMenu->addEntry(typeMenuSub, rootNames[r].c_str());
+            }
+
+            addView(hierMenu);
+
         }
 
         ~CChordSelecter()
         {
-            editor->release();
+            if (editor) editor->release();
         }
 
-        CLASS_METHODS(CChordSelecter, CViewContainer)
+        //CLASS_METHODS(CChordSelecter, CViewContainer)
 
     protected:
 
         VST3Editor* editor{};
-
-        void onNoteButtonClicked(CControl* pControl, CPoint where, bool onoff)
-        {
-            if (auto message = Steinberg::owned(editor->getController()->allocateMessage()))
-            {
-                CNoteMsg note;
-
-                ParamID tag = pControl->getTag();
-
-                note.tag = tag;
-                note.isOn = onoff;
-
-                int vel = (223 - where.x) * 2;
-                if (vel > 127) vel = 127;
-                if (vel < 1) vel = 1;
-                note.velocity = vel;
-
-                // プラグインプロセッサにノートメッセージを送信
-                message->setMessageID(MSG_SOUND_CHECK);
-                if (auto attr = message->getAttributes())
-                {
-                    attr->setBinary(MSG_SOUND_CHECK, &note, sizeof(CNoteMsg));
-                }
-                if (editor->getController() == nullptr) return;
-                editor->getController()->getPeer()->notify(message);
-            }
-        }
+        COptionMenu* hierMenu = nullptr;
+        std::unique_ptr<ChordSelectorListner> menuListener;
     };
 
     class CChordSelecterFactory
@@ -77,7 +149,7 @@ namespace MinMax
 
         CView* create(const UIAttributes& attributes, const IUIDescription* description) const override
         {
-            return new CChordSelecter(attributes, description, CRect(CPoint(0, 0), CPoint(200, 15)));
+            return new CChordSelecter(attributes, description, CRect(CPoint(0, 0), CPoint(200, 20)));
         }
     };
 
--- source/cfretboardviewfactory.cpp
+++ source/cfretboardviewfactory.cpp
@@ -18,77 +18,6 @@ namespace MinMax
     using namespace Steinberg;
     using namespace Steinberg::Vst;
 
-    class ChordSelectorListner
-        : public OptionMenuListenerAdapter
-    {
-    public:
-        ChordSelectorListner(CFretBoard* fretborad, VST3Editor* editor)
-            : fretBoard(fretborad)
-            , editor(editor)
-        {
-            if (fretBoard)
-            {
-                CChord chord{};
-                fretBoard->setValue(chord);
-            }
-        }
-
-        bool onOptionMenuSetPopupResult(COptionMenu* menu, COptionMenu* selectedMenu, int32_t selectedIndex) override
-        {
-            if (!menu || !selectedMenu) return false;
-
-            CChord chord;
-
-            bool finish = false;
-            int chordNumber = 0; // 先頭からの位置インデックス
-
-            for (int r = 0; r < menu->getNbEntries(); ++r)
-            {
-                COptionMenu* typeMenu = menu->getSubMenu(r);
-                if (!typeMenu) continue;
-
-                for (int t = 0; t < typeMenu->getNbEntries(); ++t)
-                {
-                    COptionMenu* posMenu = typeMenu->getSubMenu(t);
-                    if (!posMenu) continue;
-
-                    if (posMenu == selectedMenu)
-                    {
-                        chord.root = r;
-                        chord.type = t;
-                        chord.position = selectedIndex;
-
-                        fretBoard->setValue(chord);
-
-                        chordNumber += selectedIndex;
-
-                        finish = true;
-                    }
-                    if (finish) break;
-                    chordNumber += posMenu->getNbEntries();
-                }
-                if (finish) break;
-            }
- 
-            if (finish)
-            {
-                EditController* controller = dynamic_cast<EditController*>(editor->getController());
-                if (!controller) return false;
-                controller->beginEdit(1104);
-                ParamValue norm = controller->plainParamToNormalized(1104, chordNumber);
-                controller->setParamNormalized(1104, norm);
-                controller->performEdit(1104, norm);
-                controller->endEdit(1104);
-            }
-
-            return finish;
-        }
-
-    private:
-        CFretBoard* fretBoard = nullptr;
-        VST3Editor* editor = nullptr;
-    };
-
     class CFretBoardView
         : public CViewContainer
     {
@@ -97,79 +26,25 @@ namespace MinMax
             : CViewContainer(size)
         {
             editor = dynamic_cast<VST3Editor*>(description->getController());
-            //if (editor) editor->addRef();
+            if (editor) editor->addRef();
 
             setBackgroundColor(kGreyCColor);
 
             CFretBoard* fretBoard = new CFretBoard(CRect(0, 20, 1120, size.getHeight() + 20));
             fretBoard->numStrings = STRING_COUNT;
             addView(fretBoard);
-
-            // 階層化メニュー
-            hierMenu = new COptionMenu(CRect(400, 1, 429, 18), nullptr, -1);
-            menuListener = std::make_unique<ChordSelectorListner>(fretBoard, editor);
-
-            hierMenu->setFontColor(kWhiteCColor);
-            hierMenu->setBackColor(kGreyCColor);
-            hierMenu->registerOptionMenuListener(menuListener.get());
-
-            // ChordMap から階層データを追加
-            const auto& chordMap = ChordMap::Instance();
-            auto rootNames = chordMap.getRootNames();
-
-            for (int r = 0; r < (int)rootNames.size(); ++r)
-            {
-                COptionMenu* typeMenuSub = new COptionMenu(CRect(0, 0, 150, 20), nullptr, -1);
-
-                auto chordNames = chordMap.getChordNames(r);
-                for (int t = 0; t < (int)chordNames.size(); ++t)
-                {
-                    COptionMenu* posMenuSub = new COptionMenu(CRect(0, 0, 150, 20), nullptr, -1);
-
-                    auto posNames = chordMap.getFretPosNames(r, t);
-                    for (auto& posName : posNames)
-                    {
-                        posMenuSub->addEntry(posName.c_str());
-                    }
-
-                    typeMenuSub->addEntry(posMenuSub, chordNames[t].c_str());
-                }
-
-                hierMenu->addEntry(typeMenuSub, rootNames[r].c_str());
-            }
-
-            addView(hierMenu);
-
- /*           CChordLabel* labChord = new CChordLabel(CRect(430, 1, 549, 18));
-            labChord->setFont(kNormalFontSmall);
-            labChord->setListener(editor);
-            labChord->setTag(1104);
-            addView(labChord);*/
         }
-
-        CFretBoardView(const CFretBoardView&) = delete;
 
         ~CFretBoardView()
         {
-            menuListener.reset();
+            if (editor) editor->release();
         }
 
-        bool removed(CView* parent) override
-        {
-            if (hierMenu && menuListener)
-            {
-                hierMenu->unregisterOptionMenuListener(menuListener.get());
-            }
-            return CViewContainer::removed(parent);
-        }
-
-        //CLASS_METHODS(CFretBoardView, CViewContainer)
+        CLASS_METHODS(CFretBoardView, CViewContainer)
 
     private:
 
         VST3Editor* editor = nullptr;;
-        std::unique_ptr<ChordSelectorListner> menuListener;
-        COptionMenu* hierMenu = nullptr;
     };
 
     class CFretBoardViewFactory
