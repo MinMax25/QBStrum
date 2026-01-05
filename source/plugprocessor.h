4b884c2376d609e1694e5ec01920ce09eb2e2f54
MinMax25 <kinocta@qa2.so-net.ne.jp>
2026-01-05 19:14:42 +0900

    reg

--- source/plugprocessor.h
+++ source/plugprocessor.h
@@ -15,9 +15,10 @@
 #include <public.sdk/source/vst/vstaudioeffect.h>
 
 #include "chordmap.h"
-#include "plugdefine.h"
-#include "eventscheduler.h"
+#include "parameterframework.h"
 #include "myparameters.h"
+#include "eventscheduler.h"
+#include "plugdefine.h"
 
 namespace MinMax
 {
